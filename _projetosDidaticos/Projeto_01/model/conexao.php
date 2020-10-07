4e73530252301b10f2090076e8dbe26af5e369a3
Leonardo T. S <66446331+LeonardoTS-ETEC-Barueri@users.noreply.github.com>
2020-10-07 00:22:14 -0300

    Conexão com a base de dados realizada com sucesso!

---
+++ _projetosDidaticos/Projeto_01/model/conexao.php
@@ -0,0 +1,26 @@
+<?php
+
+$dbServidor = '127.0.0.1';
+$dbUsuario = 'root';
+$dbSenha = '';
+$dbBanco = 'pw2_db_projeto01';
+
+$conn = mysqli_connect($dbServidor, $dbUsuario, $dbSenha, $dbBanco);
+
+$sql = "SET NAMES 'utf8'";
+mysqli_query($conn, $sql);
+$sql = "SET character_set_connection=utf8";
+mysqli_query($conn, $sql);
+$sql = "SET character_set_client=utf8";
+mysqli_query($conn, $sql);
+$sql = "SET character_set_results=utf8";
+mysqli_query($conn, $sql);
+
+if (mysqli_connect_error($conn)){
+    echo ("Erro na conexão: " . mysqli_connect_error());
+    exit();
+} else {
+    echo "Conectou!";
+}
+
+?>
